2fba78ad4fcf3e242acfd0a88cd158a824c79883
Daniel <danieldiastsany141@gmail.com>
2025-12-01 09:08:13 +0700

    TestisKuda

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,8 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-// GANTI DENGAN DATA DARI SUPABASE-MU!
-const supabaseUrl = 'https://bvelvqjfnwqnascpjxaj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2ZWx2cWpmbndxbmFzY3BqeGFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3OTcxMjIsImV4cCI6MjA3OTM3MzEyMn0.HW31isQGBrlOGG_KQlo6JoN72qqr4jHTGdl6cguYjpQ';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
